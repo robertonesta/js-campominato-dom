87a8760948b93d380457ddddd2d2f2ea93f81062
RobertoNesta <robertonesta1993@gmail.com>
2023-03-20 15:07:23 +0100

    generating the random numbers array

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,18 @@ const containerEl = document.querySelector(".container");
 
 const playEl = document.querySelector(".play")
 
-playEl.addEventListener('click', function(e){
-  e.preventDefault();
 
+playEl.addEventListener('click', function(){
   containerEl.innerHTML = "";
 
   const difficultyEl = document.getElementById("difficulty")
   console.log(difficultyEl)
-  //creare una funzione in cui l'nMax varia a seconda della difficoltà
-
+  //creare una variabile in cui l'nMax varia a seconda della difficoltà
   let nMax = difficultyEl.value;
-
+   
   // creo un ciclo n volte per le celle (dove n in questo caso sta per 100 o 81 o 49)
   for (let i = 1; i <= nMax; i++) {
-  const cell = `<div class="cell" style="width: calc(100% / ${Math.sqrt(nMax)}"><p class="m-0">${i}</p></div>`;
+  const cell = `<div class="cell" style="width: calc(100% / ${Math.sqrt(nMax)}"><p class="m-0">${i}</p>${bombs}</div>`;
   containerEl.innerHTML += cell;
   }
   // seleziono una cella che ha classe cell e active
@@ -30,11 +28,34 @@ playEl.addEventListener('click', function(e){
         thisCell.classList.add("bg_selected")
         // this.classList.add("active")
         console.log("Changed the color")
-    })
-}
-})
+        })
+    }
+    //creare una variabile per generare le bombe
+   const bombs = createBombs(nMax);
+   //creare variabile per segnalare il punteggio
+   let points = 0;
+   //creare funzione per le bombe
+   function createBombs(cellEl){
+     const bombsRandomNumber = [];
+ 
+   //-generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+   for (let i = 0; i < 16; i++) {
+     let bomb = Math.floor(Math.random() * nMax) + 1;
+ 
+     // controllare se i numeri generati sono nell'array dei numeri-bombe
+     while (bombsRandomNumber.includes(bomb)) {
+       bomb = Math.floor(Math.random() * nMax) + 1;
+     }
+     bombsRandomNumber.push(bomb);
+   }
+   //inserisci le bombe nell'array
+   bombsRandomNumber.sort((a, b) => a - b);
+   console.log(bombsRandomNumber);
+   }
+   
+  })
+  
 
-//-generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 //nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 //Se si clicca su una cella che è una bomba: la cella si colora di rosso e finisce la partita.
 //Altrimenti si colora di azzurro e si può continuare a cliccare sulle altre celle.
