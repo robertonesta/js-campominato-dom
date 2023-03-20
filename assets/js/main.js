758b5af79bf7d6aec6a0547c931957cbd7ad7ead
RobertoNesta <robertonesta1993@gmail.com>
2023-03-20 17:38:58 +0100

    adding the final score as alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,8 @@ playEl.addEventListener('click', function () {
     //creare una variabile in cui l'nMax varia a seconda della difficoltà
     let nMax = difficultyEl.value;
     const bombs = createBombs(nMax);
+    //creare variabile per segnalare il punteggio
+    let points = 0;
 
     // creo un ciclo n volte per le celle (dove n in questo caso sta per 100 o 81 o 49)
     for (let i = 1; i < nMax; i++) {
@@ -22,31 +24,35 @@ playEl.addEventListener('click', function () {
 
     const cellEl = document.querySelectorAll(".cell")
 
-    for (let i = 1; i < cellEl.length; i++) {
+    for (let i = 0; i < cellEl.length; i++) {
         const thisCell = cellEl[i];
-        console.log(thisCell)
-        let isABomb = bombs.includes(i);
-        console.log(isABomb)
+        //console.log(thisCell)       
+        
         thisCell.addEventListener("click", function () {
+            //Se si clicca su una cella che è una bomba: la cella si colora di rosso e finisce la partita.
+            console.log(i)
+            let isABomb = bombs.includes(i+1);
             if (!isABomb) {
                 thisCell.classList.add("bg_selected")
+                points++;
                 // this.classList.add("active")
-                console.log("Changed the color")
+                //console.log("Changed the color")
             } else {
-                thisCell.classList.add("bg_bomb")               
+                thisCell.classList.add("bg_bomb")
+                setTimeout(() => {
+                    alert(`Hai perso! Il tuo punteggio finale è ${points}`);
+                }, 1);                            
             }
         })
     }
     //creare una variabile per generare le bombe
     console.log(bombs);
-    //creare variabile per segnalare il punteggio
-    let points = 0;
     //creare funzione per le bombe
     function createBombs(cellEl) {
         const bombsRandomNumber = [];
 
         //-generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-        for (let i = 1; i < 16; i++) {
+        for (let i = 0; i < 16; i++) {
             let bomb = Math.floor(Math.random() * nMax) + 1;
 
             // controllare se i numeri generati sono nell'array dei numeri-bombe
@@ -59,11 +65,5 @@ playEl.addEventListener('click', function () {
         bombsRandomNumber.sort((a, b) => a - b);
         return bombsRandomNumber;
     }
-    //Se si clicca su una cella che è una bomba: la cella si colora di rosso e finisce la partita.
-
 })
-
-//Altrimenti si colora di azzurro e si può continuare a cliccare sulle altre celle.
-//la partita finisce o quando si clicca su una bomba o quando si cliccano tutte le celle che non sono bombe.
-//a fine partita il software genera il punteggio finale delle celle blu cliccate.
 
